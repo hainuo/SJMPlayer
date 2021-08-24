9784ff98c504e693ae2654722d139fe4c232a432
hainuo <admin@hainuo.info>
2021-08-24 08:49:06 +0800

    Initial Commit

---
+++ SJMPlayer/SJMPlayer.h
@@ -0,0 +1,12 @@
+//
+//  SJMPlayer.h
+//  SJMPlayer
+//
+//  Created by hainuo on 2021/8/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SJMPlayer : NSObject
+
+@end
---
+++ SJMPlayer/SJMPlayer.m
@@ -0,0 +1,12 @@
+//
+//  SJMPlayer.m
+//  SJMPlayer
+//
+//  Created by hainuo on 2021/8/24.
+//
+
+#import "SJMPlayer.h"
+
+@implementation SJMPlayer
+
+@end
